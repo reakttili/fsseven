14110d5696ff38d1dd2b9f8d1221932689579baf
Ville Byman <villebyman@gmail.com>
2018-09-15 14:41:12 +0300

    Remove cross-env for test

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ mongoose.connect(config.mongoUrl)
 
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogRouter)
